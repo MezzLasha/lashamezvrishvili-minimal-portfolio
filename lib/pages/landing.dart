86c47bbc88f9fa7c83a4dbe9d0cfa630385f2fbc
MezzLasha <mezzlasha@gmail.com>
2023-03-22 15:16:04 +0400

    change to horizontal scroll and remove background

--- lib/pages/landing.dart
+++ lib/pages/landing.dart
@@ -41,8 +41,8 @@ class _LandingPageState extends State<LandingPage>
     return Scaffold(
         body: SingleChildScrollView(
       controller: _scrollController,
-      physics: const BouncingScrollPhysics(),
-      child: Column(
+      scrollDirection: Axis.horizontal,
+      child: Row(
         children: [
           SizedBox(
             height: MediaQuery.of(context).size.height,
@@ -50,96 +50,79 @@ class _LandingPageState extends State<LandingPage>
             child: Stack(
               children: [
                 Center(
-                  child: Container(
-                    height: MediaQuery.of(context).size.height - 200,
-                    width: MediaQuery.of(context).size.width - 200,
-                    decoration: BoxDecoration(
-                      image: const DecorationImage(
-                        image: AssetImage('assets/images/vacancies.png'),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: SmoothRectangleBorder(
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(20)),
-                              smoothness: 0.6)
-                          .borderRadius,
-                    ),
-                    child: BlendMask(
-                      blendMode: BlendMode.colorBurn,
-                      child: MouseRegion(
-                        onEnter: (event) {
-                          _controller.forward();
-                        },
-                        onExit: (event) {
-                          _controller.reverse();
-                        },
-                        cursor: SystemMouseCursors.click,
-                        child: GestureDetector(
-                          onTap: () {
-                            _scrollController.animateTo(
-                                MediaQuery.of(context).size.height,
-                                duration: const Duration(milliseconds: 500),
-                                curve: Curves.easeInOutCubicEmphasized);
-                          },
-                          child: AnimatedBuilder(
-                            animation: _animation,
-                            builder: (context, child) {
-                              return Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Transform.translate(
-                                      offset:
-                                          Offset(-20 * (_animation.value), 0),
-                                      child: child!),
-                                  Transform.translate(
-                                    offset: Offset(20 * (_animation.value), 0),
-                                    child: Opacity(
-                                      opacity: _animation.value,
-                                      child: const Icon(Icons.play_arrow,
-                                          size: 80,
-                                          color:
-                                              Color.fromARGB(255, 36, 10, 168)),
-                                    ),
+                  child: MouseRegion(
+                    onEnter: (event) {
+                      _controller.forward();
+                    },
+                    onExit: (event) {
+                      _controller.reverse();
+                    },
+                    cursor: SystemMouseCursors.click,
+                    child: InkWell(
+                      onTap: () {
+                        _scrollController.animateTo(
+                            _scrollController.position.maxScrollExtent,
+                            duration: const Duration(milliseconds: 500),
+                            curve: Curves.easeInOutCubicEmphasized);
+                      },
+                      hoverColor: Colors.transparent,
+                      splashColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                      focusColor: Colors.transparent,
+                      child: AnimatedBuilder(
+                        animation: _animation,
+                        builder: (context, child) {
+                          return Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Transform.translate(
+                                  offset: Offset(-20 * (_animation.value), 0),
+                                  child: child!),
+                              Transform.translate(
+                                offset: Offset(20 * (_animation.value), 0),
+                                child: Opacity(
+                                  opacity: _animation.value,
+                                  child: const Icon(
+                                    Icons.play_arrow,
+                                    size: 80,
                                   ),
-                                ],
-                              );
-                            },
-                            child: Transform.translate(
-                              offset: const Offset(40, 0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                mainAxisSize: MainAxisSize.min,
-                                children: const [
-                                  Text(
-                                    'ME',
-                                    maxLines: 2,
-                                    overflow: TextOverflow.visible,
-                                    style: TextStyle(
-                                      fontSize: 80,
-                                      height: 0.8,
-                                      letterSpacing: -20,
-                                      color: Color.fromARGB(255, 36, 10, 168),
-                                      fontFamily: 'Neue',
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    'ZZ',
-                                    maxLines: 2,
-                                    overflow: TextOverflow.visible,
-                                    style: TextStyle(
-                                      fontSize: 80,
-                                      height: 0.75,
-                                      color: Color.fromARGB(255, 36, 10, 168),
-                                      letterSpacing: 0,
-                                      fontFamily: 'Neue',
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ],
+                                ),
                               ),
-                            ),
+                            ],
+                          );
+                        },
+                        child: Transform.translate(
+                          offset: const Offset(40, 0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: const [
+                              Text(
+                                'ME',
+                                maxLines: 2,
+                                overflow: TextOverflow.visible,
+                                style: TextStyle(
+                                  fontSize: 80,
+                                  height: 0.8,
+                                  letterSpacing: -20,
+                                  fontFamily: 'Neue',
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                'ZZ',
+                                maxLines: 2,
+                                overflow: TextOverflow.visible,
+                                style: TextStyle(
+                                  fontSize: 80,
+                                  height: 0.75,
+                                  letterSpacing: 0,
+                                  fontFamily: 'Neue',
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ),
@@ -161,10 +144,25 @@ class _LandingPageState extends State<LandingPage>
                     ),
                   ),
                 ),
+                // const Align(
+                //   alignment: Alignment.bottomCenter,
+                //   child: Padding(
+                //     padding: EdgeInsets.only(bottom: 35),
+                //     child: Text(
+                //       'Flutter Developer',
+                //       style: TextStyle(
+                //           fontFamily: 'NeueMontreal',
+                //           fontSize: 20,
+                //           letterSpacing: -1.2,
+                //           color: Color.fromARGB(255, 36, 10, 168),
+                //           fontWeight: FontWeight.w500),
+                //     ),
+                //   ),
+                // ),
               ],
             ),
           ),
-          const Divider(),
+          const VerticalDivider(),
           const HomePage(),
         ],
       ),
