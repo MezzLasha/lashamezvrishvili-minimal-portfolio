ba92ca33b94d51d461c3b9f7225a304c3124f8ec
MezzLasha <mezzlasha@gmail.com>
2023-03-27 12:43:09 +0400

    populate project pages

--- lib/pages/aggregate.dart
+++ lib/pages/aggregate.dart
@@ -7,13 +7,15 @@ import 'package:smooth_corner/smooth_corner.dart';
 import '../widgets/project_widgets.dart';
 
 const aggregateScreenshots = {
-  'assets/images/aggregate_images/1.png',
-  'assets/images/aggregate_images/2.png',
-  'assets/images/aggregate_images/3.png',
-  'assets/images/aggregate_images/4.png',
-  'assets/images/aggregate_images/5.png',
-  'assets/images/aggregate_images/6.png',
-  'assets/images/aggregate_images/7.png',
+  'assets/images/aggregate_images/1.jpg',
+  'assets/images/aggregate_images/2.jpg',
+  'assets/images/aggregate_images/3.jpg',
+  'assets/images/aggregate_images/4.jpg',
+  'assets/images/aggregate_images/5.jpg',
+  'assets/images/aggregate_images/6.jpg',
+  'assets/images/aggregate_images/7.jpg',
+  'assets/images/aggregate_images/8.jpg',
+  'assets/images/aggregate_images/9.jpg',
 };
 
 class AggregatePage extends StatelessWidget {
@@ -113,127 +115,26 @@ class AggregatePage extends StatelessWidget {
                   height: 16,
                 ),
                 RichText(
-                  text: TextSpan(
-                    style: const TextStyle(
+                  text: const TextSpan(
+                    style: TextStyle(
                         fontFamily: 'Neue', color: Colors.white, fontSize: 16),
                     children: [
-                      const TextSpan(
-                          text:
-                              'The Vacancies app is a mobile application that can be downloaded from the Google Play Store for Android users. It\'s designed to help you find and apply to your dream job with ease. \n\n'),
-                      const TextSpan(
-                          text:
-                              'The app supports automatic resume attachments, allowing you to apply to multiple job openings without having to search through multiple websites. The app uses web scraping technology to gather all the latest job advertisements from other websites and present them to you in one convenient location. \n\n'),
-                      const TextSpan(
-                          text:
-                              'It\'s built with Flutter and uses BLoC (Business Logic Component) and Stateful state management to provide a smooth and seamless experience. The app is integrated with Firebase Crash Analytics and Performance Monitoring. It\'s designed with the '),
                       TextSpan(
-                          text: 'Material You',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w200,
-                              fontStyle: FontStyle.italic,
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .onSurfaceVariant)),
-                      const TextSpan(
                           text:
-                              ' design language, ensuring a modern and intuitive interface for our users. \n\n'),
-                      const TextSpan(
+                              'Aggregate is a mobile application for Android and iOS users. It\'s designed to help you trade your solana assets efficiently. \n\n'),
+                      TextSpan(
                           text:
-                              'Whether you\'re a seasoned professional or just starting out, the Vacancies app is the perfect tool to help you land your dream job. Give it a try today!'),
+                              'With Aggregate, you can easily explore new NFTs, track your favorite Collectibles, sell and buy NFTs and keep track of your wallet and your trading performance. \n\n'),
+                      TextSpan(
+                          text:
+                              'Built with Flutter, Aggregate is a cross-platform application that can be used on both Android and iOS devices. It integrates Phantom Solana mobile wallet with deep-linking. \n\n'),
+                      TextSpan(
+                          text:
+                              'App Uses firebase for crashalytics and performance monitoring, and CoralCube, MagicEden and Hyperspace Rest APIs for seamless user experience between different platforms.  \n\n'),
                     ],
                   ),
                 )
               ],
-            ),
-            const SizedBox(
-              height: 48,
-            ),
-            const HeaderWidget(
-              title: 'Check it out on\nGoogle Play Store.',
-              style: TextStyle(
-                  fontFamily: 'Neue',
-                  color: Color(0xffd5fcc1),
-                  fontSize: 30,
-                  fontWeight: FontWeight.w900),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            AspectRatio(
-                aspectRatio: 1.734,
-                child: InkWell(
-                  onTap: () async {
-                    await launch(
-                        'https://play.google.com/store/apps/details?id=com.mezzlasha.vacancy');
-                  },
-                  customBorder: SmoothRectangleBorder(
-                    smoothness: 0.6,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Stack(
-                    children: [
-                      Positioned.fill(
-                        child: Ink(
-                          decoration: BoxDecoration(
-                            borderRadius: SmoothRectangleBorder(
-                              smoothness: 0.6,
-                              borderRadius: BorderRadius.circular(12),
-                            ).borderRadius,
-                            image: const DecorationImage(
-                                image: AssetImage(
-                                    'assets/images/aggregate_images/link_google_play_preview.png'),
-                                fit: BoxFit.cover),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: SmoothRectangleBorder(
-                            smoothness: 0.6,
-                            borderRadius: BorderRadius.circular(12),
-                          ).borderRadius,
-                          gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [
-                              Colors.transparent,
-                              Colors.black.withOpacity(0.2),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: const [
-                              Icon(
-                                Icons.link,
-                                color: Colors.black,
-                              ),
-                              SizedBox(
-                                width: 8,
-                              ),
-                              Text(
-                                'play.google.com',
-                                style: TextStyle(
-                                    fontFamily: 'Neue',
-                                    color: Colors.black,
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                    fontStyle: FontStyle.italic),
-                              ),
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                )),
-            const SizedBox(
-              height: 24,
             ),
           ],
         ));
--- lib/pages/delauneymx.dart
+++ lib/pages/delauneymx.dart
@@ -114,36 +114,22 @@ class DelauneyPage extends StatelessWidget {
                   height: 16,
                 ),
                 RichText(
-                  text: TextSpan(
-                    style: const TextStyle(
+                  text: const TextSpan(
+                    style: TextStyle(
                         fontFamily: 'Neue', color: Colors.white, fontSize: 16),
                     children: [
-                      const TextSpan(
-                          text:
-                              'The Vacancies app is a mobile application that can be downloaded from the Google Play Store for Android users. It\'s designed to help you find and apply to your dream job with ease. \n\n'),
-                      const TextSpan(
-                          text:
-                              'The app supports automatic resume attachments, allowing you to apply to multiple job openings without having to search through multiple websites. The app uses web scraping technology to gather all the latest job advertisements from other websites and present them to you in one convenient location. \n\n'),
-                      const TextSpan(
-                          text:
-                              'It\'s built with Flutter and uses BLoC (Business Logic Component) and Stateful state management to provide a smooth and seamless experience. The app is integrated with Firebase Crash Analytics and Performance Monitoring. It\'s designed with the '),
                       TextSpan(
-                          text: 'Material You',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w200,
-                              fontStyle: FontStyle.italic,
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .onSurfaceVariant)),
-                      const TextSpan(
                           text:
-                              ' design language, ensuring a modern and intuitive interface for our users. \n\n'),
-                      const TextSpan(
+                              'This app was developed for Delauney.mx. A Shampoo and health-care company located in mexico. \n\n'),
+                      TextSpan(
                           text:
-                              'Whether you\'re a seasoned professional or just starting out, the Vacancies app is the perfect tool to help you land your dream job. Give it a try today!'),
+                              'App was developed using Flutter, Firebase and php(server/mysql). \n\n'),
+                      TextSpan(
+                          text:
+                              'TimeFrame: 1.5 months. \nDelivered Products: Mobile App for iOS and Android, Administrator panel, Backend made in php / mySQL on a dedicated VPS. \n'),
                     ],
                   ),
-                )
+                ),
               ],
             ),
             const SizedBox(
@@ -219,6 +205,93 @@ class DelauneyPage extends StatelessWidget {
                               ),
                               Text(
                                 'play.google.com',
+                                style: TextStyle(
+                                    fontFamily: 'Neue',
+                                    color: Colors.black,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                    fontStyle: FontStyle.italic),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )),
+            const SizedBox(
+              height: 48,
+            ),
+            HeaderWidget(
+              title: 'Check it out on\nApp Store.',
+              style: TextStyle(
+                  fontFamily: 'Neue',
+                  color: Colors.blue[100],
+                  fontSize: 30,
+                  fontWeight: FontWeight.w900),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            AspectRatio(
+                aspectRatio: 1.934,
+                child: InkWell(
+                  onTap: () async {
+                    await launch(
+                        'https://apps.apple.com/us/app/delauney/id6446408684');
+                  },
+                  customBorder: SmoothRectangleBorder(
+                    smoothness: 0.6,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: Ink(
+                          decoration: BoxDecoration(
+                            borderRadius: SmoothRectangleBorder(
+                              smoothness: 0.6,
+                              borderRadius: BorderRadius.circular(12),
+                            ).borderRadius,
+                            image: const DecorationImage(
+                                image: AssetImage(
+                                    'assets/images/delauneymx_images/link_appstore_preview.png'),
+                                fit: BoxFit.cover),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: SmoothRectangleBorder(
+                            smoothness: 0.6,
+                            borderRadius: BorderRadius.circular(12),
+                          ).borderRadius,
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Colors.transparent,
+                              Colors.black.withOpacity(0.2),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: const [
+                              Icon(
+                                Icons.link,
+                                color: Colors.black,
+                              ),
+                              SizedBox(
+                                width: 8,
+                              ),
+                              Text(
+                                'apps.apple.com',
                                 style: TextStyle(
                                     fontFamily: 'Neue',
                                     color: Colors.black,
